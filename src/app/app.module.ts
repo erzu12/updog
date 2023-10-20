5291cff8b3deefdca96b9b0f5b9dc57c81c4d019
Alexander Wyss <alexsilvan.wyss@gmail.com>
2023-10-20 20:29:08 +0200

    add angular firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'updog-25782',
+    appId: '1:66523967927:web:f69fff2400b860ab8bf832',
+    storageBucket: 'updog-25782.appspot.com',
+    apiKey: 'AIzaSyCeAfXWW_YkNX0YbK9Q4PLd0fRkCwBDznY',
+    authDomain: 'updog-25782.firebaseapp.com',
+    messagingSenderId: '66523967927',
+    measurementId: 'G-ZMS401FRRJ',
+  },
   production: false
 };
 
